36bad9fc759e4077a63f8b4c49722ac8493a4f09
sharan2000 <chitrasharan2000@gmail.com>
2022-11-18 18:26:50 +0530

    first commit

---
+++ PPMTool/src/main/java/com/randomcompany/ppmtool/PpmToolApplication.java
@@ -0,0 +1,13 @@
+package com.randomcompany.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmToolApplication.class, args);
+	}
+
+}
---
+++ PPMTool/src/test/java/com/randomcompany/ppmtool/PpmToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.randomcompany.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
